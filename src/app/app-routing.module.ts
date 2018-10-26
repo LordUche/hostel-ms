2638d55633e0e6de9cc4b9f7f0d89a4b81fc6773
Uchenna A. Iheanacho <uchenna.a.i@gmail.com>
2018-10-26 02:04:38 +0100

    setup routes for hostel, block and room lists

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HostelListComponent } from './core/hostel/hostel-list/hostel-list.component';
+import { BlockListComponent } from './core/block/block-list/block-list.component';
+import { RoomListComponent } from './core/room/room-list/room-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HostelListComponent },
+  { path: 'hostels', component: HostelListComponent },
+  { path: 'hostels/:id/blocks', component: BlockListComponent },
+  { path: 'blocks/:id/rooms', component: RoomListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
